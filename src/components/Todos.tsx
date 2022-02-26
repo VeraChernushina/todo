52106138227caf9d5d079421f0105d04c1b67c5e
Vera Chernushina <vera.chernushina@yandex.ru>
2022-02-27 01:32:02 +0300

    feat: create TodoItem component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 import Todo from "../models/todos";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-      {props.items.map(item => <li key={item.id}>{item.text}</li>)}
+      {props.items.map(item => <TodoItem text={item.text} key={item.id} />)}
     </ul>
   )
 };
