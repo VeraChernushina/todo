17432c4416a5db89b1e9a405396b997dfa1fb5bd
Vera Chernushina <vera.chernushina@yandex.ru>
2022-02-27 14:27:33 +0300

    feat: form to add new todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
+
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todos';
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = useState<Todo[]>([
     new Todo('Learn React'),
     new Todo('Learn TypeScript')
-  ]
+  ]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
